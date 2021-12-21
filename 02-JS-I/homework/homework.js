6bdc304fa160049bd2560a2fde3ea004002ecf0c
flortalavera <talaveraflorencia@hotmail.com>
2021-12-21 18:34:03 -0300

    modif

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = "flor";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1991;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
